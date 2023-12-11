4779d3e27c09d553297e81a1e7c0875ee6cd9b7f
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2023-12-11 15:52:36 +0700

    Update script.sh

--- lab1/script.sh
+++ lab1/script.sh
@@ -6,26 +6,26 @@ trap 'rm -rf "$temp_d"' HUP INT QUIT PIPE TERM EXIT
 
 if [ ! -f "$input" ] || [ ! -r "$input" ]; then
     echo "Cannot find or read source file!"
-    exit 2
+    exit 1
 fi
 
 output=$(grep -o "&Output:[[:space:]]*[a-zA-Z0-9_]*\.txt" "$input" | awk '{print $2}')
 
 if [ -z "$output" ]; then
     echo "Output file name not specified!"
-    exit 3
+    exit 2
 fi
 
 if [ "${input##*.}" = "cpp" ]; then
     g++ "$input" -o "$temp_d/$output"
 else
     echo "File type unsupported!"
-    exit 4
+    exit 3
 fi
 
 if [ $? -ne 0 ]; then
     echo "Failed compilation!"
-    exit 5
+    exit 4
 fi
 
 mv "$temp_d/$output" "$(dirname "$input")/$output"
