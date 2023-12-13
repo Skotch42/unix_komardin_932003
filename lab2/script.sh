e5ec1730d1f426cce5a0e91c84b50d99deb9ab5d
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2023-12-13 23:34:25 +0700

    Update script.sh

--- lab2/script.sh
+++ lab2/script.sh
@@ -3,21 +3,21 @@
 while true
 do
     (
-    flock -x 200
+    flock -x 3
 
     counter=1
+        
     while [ -e "/my_volume/$(printf "%03d" $counter).txt" ]
     do
         ((counter++))
     done
 
-    container_id=$(cat /proc/self/cgroup | grep "docker" | sed 's/^.*\///' | head -n 1)
-    echo "$container_id $counter" > "/my_volume/$(printf "%03d" $counter).txt"
+    echo "$(hostname) $counter" > "/my_volume/$(printf "%03d" $counter).txt"
 
     sleep 1
 
     rm "/my_volume/$(printf "%03d" $counter).txt"
 
-    ) 200>/var/lock/container_lock
+    ) 3>/var/lock/mylockfile
 
 done
