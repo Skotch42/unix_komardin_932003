8ed71d3f45c5888f97f3b62253065188ae33b967
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2023-12-13 23:19:50 +0700

    Update script.sh

--- lab2/script.sh
+++ lab2/script.sh
@@ -6,17 +6,17 @@ do
     flock -x 200
 
     counter=1
-    while [ -e "/shared_volume/$(printf "%03d" $counter).txt" ]
+    while [ -e "/my_volume/$(printf "%03d" $counter).txt" ]
     do
         ((counter++))
     done
 
     container_id=$(cat /proc/self/cgroup | grep "docker" | sed 's/^.*\///' | head -n 1)
-    echo "$container_id $counter" > "/shared_volume/$(printf "%03d" $counter).txt"
+    echo "$container_id $counter" > "/my_volume/$(printf "%03d" $counter).txt"
 
     sleep 1
 
-    rm "/shared_volume/$(printf "%03d" $counter).txt"
+    rm "/my_volume/$(printf "%03d" $counter).txt"
 
     ) 200>/var/lock/container_lock
 
