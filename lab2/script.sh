7d30f5b8a339cd4f019507d33e5e26e2301e6cc2
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2023-12-18 13:38:30 +0700

    Update script.sh

--- lab2/script.sh
+++ lab2/script.sh
@@ -2,8 +2,8 @@
 
 while true
 do
-    (
-    flock -x 3
+    exec 3>/var/lock/mylockfile
+    flock 3
 
     counter=1
         
@@ -11,13 +11,14 @@ do
     do
         ((counter++))
     done
+    
+    flock -u 3
+    exec 3>&-
 
     echo "$(hostname) $counter" > "/my_volume/$(printf "%03d" $counter).txt"
 
     sleep 1
 
     rm "/my_volume/$(printf "%03d" $counter).txt"
-
-    ) 3>/var/lock/mylockfile
 
 done
